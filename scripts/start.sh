913681f71c42a8607b60e25cace51c8052b2b0fc
leekt0321 <leekt0321@naver.com>
2024-06-25 08:17:01 +0000

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo cat << EOF > /etc/profile
+export CATALINA_HOME=/usr/local/tomcat
+EOF
+source /etc/profile
+sudo /usr/local/tomcat/apache-tomcat-10.1.25/bin/startup.sh
 sudo systemctl start tomcat.service
 sudo systemctl enable tomcat.service
 sudo systemctl start httpd.service
