e523a53a280cf0abbf1b658d38d6d8f2e47b1956
leekt0321 <leekt0321@naver.com>
2024-06-25 08:55:52 +0000

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo yum install java-17-amazon-corretto -y
+sudo yum install java-17-amazon-corretto-devel -y
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.25/bin/apache-tomcat-10.1.25.tar.gz
 tar xvf apache-tomcat-10.1.25.tar.gz  
 sudo mv apache-tomcat-10.1.25 /usr/local/tomcat
@@ -6,7 +8,7 @@ sudo chown -R  ec2-user:ec2-user /usr/local/tomcat
 sudo yum -y install httpd
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
-    ServerAdmin root@localhost
+    ServerAdmin ec2-user@localhost
     ServerName app.wildrydes.com
     DefaultType text/html
     ProxyRequests off
